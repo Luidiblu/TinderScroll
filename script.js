9fa7033334bdf02d00b48718d15bb9b98a32c5a9
Luidiblu <diego.luidiblu@gmail.com>
2022-04-02 22:57:19 -0300

    add script

---
+++ script.js
@@ -0,0 +1,18 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function doshit() {
+    document.getElementsByClassName("Bgc($c-like-green):a")[0].click()
+}
+
+
+counter = 0
+
+
+while (true) {
+    await sleep(500)
+    doshit();
+    counter += 1
+    console.log(${counter} likes sent)
+}
